35e98d0fe932e8f498401f5a7acaedfb499b63cc
Guilherme Fontana <guilherme.fontana@fullsoft.com.br>
2025-05-10 14:45:00 -0300

    Add sign-in page

---
+++ src/app/(public)/sign-in/page.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import "./style.css"
+import React from "react";
+import { useRouter } from 'next/navigation'
+import InputText from "@/components/InputText"
+import Button from "@/components/Button";
+
+export default function Login() {
+  const router = useRouter();
+  
+  const [loading, setLoading] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [emailError, setEmailError] = React.useState(false);
+  const [password, setPassword] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState(false);
+
+  const handleClick = async () => {
+    if (!email || !password) {
+      if (!email)
+        setEmailError(true);
+      
+      if (!password)
+        setPasswordError(true);
+      
+      return;
+    }
+
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    router.push("/");
+  };
+
+  return (
+    <div className="sign-in-container">
+      <div className="sign-in-bg"></div>
+      <div className="sign-in-bg sign-in-bg3"></div>
+      <div className="sign-in-bg sign-in-bg2"></div>
+
+      <div className="sign-in-content flex">
+        <div className="sign-in-form flex">
+          <div className="sign-in-title flex">
+            <strong>Bem Vindo ao SGAMCE</strong>
+            <span className="sign-in-text">Insira suas credencias para começarmos</span>
+          </div>
+          <InputText 
+            type="email" 
+            placeholder="Email"
+            value={email}
+            error={emailError}
+            helperText="Informe o email"
+            onChange={(event) => {
+              setEmail(event.target.value)
+
+              if (event.target.value && emailError) 
+                setEmailError(false)
+            }}
+            required
+          />
+          <InputText 
+            type="password" 
+            placeholder="Password" 
+            helperText="Informe a senha"
+            value={password}
+            error={passwordError}
+            onChange={(event) => {
+              setPassword(event.target.value)
+
+              if (event.target.value && passwordError) 
+                setPasswordError(false)
+            }}
+            required 
+          />
+          <Button className="sign-in-button" textContent="Entrar" isLoading={loading} onClick={handleClick}/>
+          <div className="sign-in-without-user-text flex">
+            <span className="sign-in-text">Não tem usuário?</span>
+            <span className="sign-in-text">Contate um administrador para que ele crie um para você</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
