3e610a1a3426004908f704cfb44fc5aeaa9232e3
Guilherme Fontana <guilherme.fontana@fullsoft.com.br>
2025-05-10 14:43:31 -0300

    add globals layout and middleware

---
+++ src/middleware.ts
@@ -0,0 +1,56 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = [
+  { path: '/sign-in', whenAuthenticated: 'next' },
+  { path: '/redirect', whenAuthenticated: 'redirect' },
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATE_ROUTE = '/sign-in';
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find(route => route.path === path )
+  const authToken = request.cookies.get('token')
+  
+  if (!authToken && publicRoute) {
+   console.log(1);
+    return NextResponse.next();
+  }
+
+  if (!authToken && !publicRoute) {
+
+    const redirectUrl = request.nextUrl.clone();
+    
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATE_ROUTE;
+
+    return NextResponse.redirect(redirectUrl);
+  }
+  
+  if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
+    const redirectUrl = request.nextUrl.clone();
+    
+    redirectUrl.pathname = '/';
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (authToken && !publicRoute) {
+    //Checar ser o JWT esta expirado
+    //Se sim, redirecionar para o SignIn
+  
+    return NextResponse.next();
+  }
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
