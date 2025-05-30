0c4cb5159084779b85e44ed83bf9eba508447fcc
Guilherme Fontana <guilherme.fontana@fullsoft.com.br>
2025-05-30 10:02:10 -0300

    add error to Combo component

--- src/components/Combo/index.tsx
+++ src/components/Combo/index.tsx
@@ -1,5 +1,5 @@
 import "./style.css"
-import { FormControl, InputLabel, NativeSelect } from "@mui/material";
+import { FormControl, FormHelperText, InputLabel, NativeSelect } from "@mui/material";
 
 export interface ComboProps {
   title: string
@@ -11,21 +11,26 @@ export interface ComboProps {
   onChange: (value: string | number) => void
   required?: boolean
   className?: string
+  helperText?: string
+  error?: boolean;
   emptyValue?: boolean
 }
 
 export default function Combo(props: ComboProps) {
   return (
     <FormControl className={`combo ${props.className || ""}`}>
-      <InputLabel>{props.title}{props.required && ' *'}</InputLabel>
+      <InputLabel className={!!props.error ? "error" : ""}>{props.title}{props.required && ' *'}</InputLabel>
       <NativeSelect
-        className="select"
+        className={`select${props.error ? "-error" : ""}`}
         value={props.value}
         onChange={((event) => props.onChange(event.target.value))}
       >
         {props.emptyValue && <option aria-label="None" value="" />}
         {props.valuesList.map((x, i) => <option key={`combo-option-${i}`} value={x.value}>{x.description}</option>)}
       </NativeSelect>
+      {
+        props.error && <FormHelperText className="error">{props.helperText}</FormHelperText>
+      }
     </FormControl>
   )
 }
