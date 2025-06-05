03ea750e58fc4f8307e61a85a63613160d6e7fc1
Guilherme Fontana <guilherme.fontana@fullsoft.com.br>
2025-06-04 21:32:47 -0300

    add get payload

--- src/app/(private)/(home)/page.tsx
+++ src/app/(private)/(home)/page.tsx
@@ -1,10 +1,15 @@
+'use client';
+
+import { getCookie } from "cookies-next";
 import "./style.css"
 
 export default function Home() {
+  const payload = JSON.parse(getCookie('payload')?.toString() || "{}");
+
   return (
     <div className="private-home flex">
       <span>Olá</span>
-      <strong>Guilherme Fontana</strong>
+      <strong>{payload?.nome || "Usuário"}</strong>
     </div>
   );
 }
